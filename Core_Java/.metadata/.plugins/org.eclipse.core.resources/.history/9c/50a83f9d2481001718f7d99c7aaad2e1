a6a9a4094b16587ce930d247fafc03591322ba4b
Connor Monson <connor.monson@yahoo.com>
2017-08-14 21:17:55 -0400

    Started on Friday's assignment. Also testing commiting code

---
+++ Core_Java/.metadata/.plugins/org.eclipse.core.resources/.history/9c/50a83f9d2481001718f7d99c7aaad2e1
@@ -0,0 +1,5 @@
+package com.ex.helloworld;
+
+public class HelloAbstract {
+
+}
---
+++ Core_Java/HelloWorldExample/src/com/ex/helloworld/HelloAbstract.java
@@ -0,0 +1,11 @@
+package com.ex.helloworld;
+
+public abstract class HelloAbstract {
+	
+	public abstract int math(int a, int b);
+	
+	public void doThings() {
+		System.out.println("Hello World!");
+	}
+
+}
