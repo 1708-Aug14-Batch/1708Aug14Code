dabf4ccdd70ee311a7b31ce0f313bde19f2157ff
Joshua <joshwurdemann@gmail.com>
2017-08-15 06:20:18 -0700

    Add files via upload

---
+++ Homework1/VehicleAbstract.java
@@ -0,0 +1,15 @@
+package Homework1;
+/** default method for Vehicle.
+ * 
+ * @author joshwurdemann
+ *@version 8/15/2017
+ */
+public abstract class VehicleAbstract implements Vehicle{
+
+
+
+public void startPoint() {
+	System.out.print("Starting Point");
+}
+}
+
