fa89a29229080839330645c35317f3fe99f475dd
Allan Jones <sherekhan1369@gmail.com>
2017-08-15 20:06:07 -0400

    Core Java HW so far

--- CoreJava/CoreJavaHW1/src/Q4/Nfactorial.java
+++ CoreJava/CoreJavaHW1/src/Q4/Nfactorial.java
@@ -1,14 +1,25 @@
 package Q4;
 
+import java.util.Scanner;
+
 public class Nfactorial {
 
 	public static void main(String[] args) {
 	// Factorial of n is the product of all positive descending integers
+		int input;
+		Scanner scan = new Scanner(System.in);
+		do {
+			System.out.println("Enter a number below 20");
+			input = scan.nextInt();
+		} while(input >= 20);
+		computeFactorial(input);
+	} 
+	
+	private static void computeFactorial(int num) {
 		int fact = 1;  
-		int number = 5;    
-		for(int i = 1; i <= number; i++) {    
+		for(int i = 1; i <= num; i++) {    
 			fact *= i;    
 		}    
-		System.out.println("Factorial of " + number + " is: " + fact);    
-	}  
+		System.out.println("Factorial of " + num + " is: " + fact);    
+	}
 }
