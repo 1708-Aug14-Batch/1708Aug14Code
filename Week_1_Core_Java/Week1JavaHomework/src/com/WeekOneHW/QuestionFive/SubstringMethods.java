32cd6d176f5a4b6620a4bc7169fdb7600273a517
bondster <apbonds1@gmail.com>
2017-08-15 22:03:26 -0400

    2 more problems complete for Java HW. Brings my total to 8

--- Week_1_Core_Java/Week1JavaHomework/src/com/WeekOneHW/QuestionFive/SubstringMethods.java
+++ Week_1_Core_Java/Week1JavaHomework/src/com/WeekOneHW/QuestionFive/SubstringMethods.java
@@ -4,25 +4,26 @@ import java.util.*; //Importing in the scanner
 
 public class SubstringMethods {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		String str = new String();
-		int idx = 0;
-		System.out.println("Enter a string: ");
-		str = input.nextLine();
-		System.out.println("Enter the length of the substring you wish to return: ");
+		Scanner input = new Scanner(System.in); //Creating the scanner for input
+		
+		int idx = 0; //Creating the variable that will be the endpoint of our substring method
+		
+		System.out.println("Enter a string: "); //Getting the string
+		StringBuilder str = new StringBuilder(input.nextLine()); //Created a StringBuilder that can be mutated with the substring method
+		System.out.println("Enter the length of the substring you wish to return: "); //Got the length of the substring
 		idx = input.nextInt();
-		StringBuilder newSubString = subStringMethod(str,idx);
-		System.out.println(newSubString);
+		StringBuilder newSubString = subStringMethod(str,idx); //Created another StringBuilder set to the return of the subStringMethod
+		System.out.println(newSubString); //Output the new string
 		
 	}
 	
-	static String subStringMethod(String str, int idx) {
+	static StringBuilder subStringMethod(StringBuilder str, int idx) {
+		StringBuilder temp = new StringBuilder(); //Created a temp String to run through the following loop
 
-		char[] arr = str.toCharArray();
-		for(int i = 0; i <= idx-1; i++) {
-			
+		for(int i = 0; i <= idx - 1; i++) { //For loop that increments upwards stopping at the point given by the user
+			temp.append(str.charAt(i)); //Appends the temp String, adding on to it with the char at i for the string given.
 		}
 		
-		return null;
+		return temp; //Returns the temp value for output
 	}
 }
