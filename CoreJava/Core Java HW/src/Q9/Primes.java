e5f79903e570c991d088fb2bef7869725836f3ff
koszuta <koszutan94@gmail.com>
2017-08-15 23:08:38 -0400

    Finished #8, 9

---
+++ CoreJava/Core Java HW/src/Q9/Primes.java
@@ -0,0 +1,14 @@
+package Q9;
+
+import java.util.ArrayList;
+
+public class Primes {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> nums = new ArrayList<>();
+		for (int i = 0; i < 100; i++) nums.add(i+1);
+		
+		
+	}
+}
