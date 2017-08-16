cc53dc969d5e30ea1a95f2ea0f540ddad6891bcb
Allan Jones <sherekhan1369@gmail.com>
2017-08-16 10:06:29 -0400

    Java Core HW so far

--- CoreJava/CoreJavaHW1/src/Q4/Nfactorial.java
+++ CoreJava/CoreJavaHW1/src/Q4/Nfactorial.java
@@ -15,11 +15,21 @@ public class Nfactorial {
 		computeFactorial(input);
 	} 
 	
-	private static void computeFactorial(int num) {
+	public static void computeFactorial(int num) {
 		int fact = 1;  
 		for(int i = 1; i <= num; i++) {    
 			fact *= i;    
 		}    
 		System.out.println("Factorial of " + num + " is: " + fact);    
+	}
+
+	// for purposes of JUnit test
+	public int computeFactorial2(int num) {
+		int fact = 1;  
+		for(int i = 1; i <= num; i++) {    
+			fact *= i;    
+		}
+		return fact;
+		//System.out.println("Factorial of " + num + " is: " + fact);    
 	}
 }
