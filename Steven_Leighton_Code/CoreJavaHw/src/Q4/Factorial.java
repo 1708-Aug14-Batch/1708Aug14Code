e2abc91cc91f5ab5e5a18cc2e07307c897dc1081
Steven Leighton <steven.leighton95@gmail.com>
2017-08-15 22:26:08 -0400

    first 5 hw problems in corejava done

---
+++ Steven_Leighton_Code/CoreJavaHw/src/Q4/Factorial.java
@@ -0,0 +1,18 @@
+package Q4;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) System.out.println(i + "! = " + factorial(i));
+	}
+
+	/*
+	 * recursively multiply every number up to n
+	 * base case <= 1
+	 */
+	
+	static int factorial(int n){
+		if(n <= 1) return 1;
+		return n*factorial(n-1);
+	}
+}
