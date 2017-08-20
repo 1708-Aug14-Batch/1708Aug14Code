2f84d70a232b873c84050809a962db444cf7f4e5
Joshua Wurdemann <joshwurdemann@gmail.com>
2017-08-20 12:16:57 -0700

    Assignment 1

---
+++ HomeWork/Assignment1/src/question6/EvenIntergerTest.java
@@ -0,0 +1,22 @@
+package question6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EvenIntergerTest {
+     public int myEven = 10;
+     public int myOdd = 3;
+     
+	@Before
+	public void setUp() throws Exception {
+	 
+	}
+
+	@Test
+	public void testIsEven() {
+		assertEquals()
+	}
+
+}
