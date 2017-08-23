7dc8d9dcb4ec81614c0c0936e7c7d399eab90d07
bondster <apbonds1@gmail.com>
2017-08-23 17:00:26 -0400

    Class code

--- Week_2_SQL/introToSQL.sql
+++ Week_2_SQL/introToSQL.sql
@@ -116,3 +116,109 @@ where ex_id = 7;
 delete from example
 where ex_id = 5;
 
+
+
+create view tracklist as 
+select * from track where lower(name) like '% for %';
+
+select * from tracklist;
+
+create view albumlist as
+select* from album where albumid between 250 and 350;
+
+
+select tr.name as trackname, al.title as "Album Title", art.name as "Artist",
+g.name as "Genre name", pl.name as "PlayList", m.name as "MediaType", ivl.unitprice,
+iv.total, c.lastname, e.lastname as "Employee name"
+from track tr
+inner join album al ON tr.albumid = al.albumid
+inner join artist art on art.artistid = al.artistid
+inner join genre g on g.genreID = tr.genreid
+inner join playlisttrack plt on plt.trackid = tr.trackid
+inner join playlist pl on plt.playlistid = pl.playlistid
+inner join mediatype m on m.mediatypeid = tr.mediatypeid
+inner join invoiceline ivl on ivl.trackid = tr.trackid
+inner join invoice iv on iv.invoiceid = ivl.invoiceid
+inner join customer c on c.customerid = iv.customerid
+inner join employee e on e.employeeid = c.supportrepid;
+
+Select cust.FIRSTNAME as CUSTNAME, emp.FIRSTNAME as EMPNAME
+from employee emp
+full join customer cust on emp.state = cust.state;
+
+--avg songs per album
+select avg(total) 
+from(select count(*) as total from track, ALBUM 
+where track.albumid = ALBUM.ALBUMID group by album.title);
+
+--Find things from genre w ascii value
+select name from track
+where ascii(substr(name, 1, 1)) = 72;
+
+--
+select name from track
+where name like '%h';
+
+--#songs per genre
+select t2.name, count(*) from track t1, genre t2
+where t1.genreid = t2.genreid
+group by t2.name;
+
+--#song per playlist
+select count(playlistID) as total, trackid
+from playlisttrack group by trackid
+order by total;
+
+--Song thing
+select t1.name, count(*) from playlist t1, playlisttrack
+where t1.playlistid = playlisttrack.playlistid
+group by t1.name;
+
+--creating index
+
+create index trackname on Track(name)
+drop index trackname;
+
+--Delete cascade
+alter table artist
+add constraint artistid
+foreign key artistid
+references album, artistid;
+on delete cascade;
+
+Alter Table album
+  add constraint artist_Cascade
+  Foreign key (artistID) references artist(artistid) on Delete cascade;
+  
+insert into example(firstname, lastname, favorite_song_id)
+values ('testing', 'test', 152);
+
+create or replace procedure add_person(
+fname in varchar2,
+lname in varchar2,
+songID in number)
+as begin
+insert into example(firstname, lastname, favorite_song_id)
+values (fname, lname, songID);
+commit;
+End add_person; 
+/
+
+
+DECLARE
+  FNAME VARCHAR2(200);
+  LNAME VARCHAR2(200);
+  SONGID NUMBER;
+BEGIN
+  FNAME := 'stored';
+  LNAME := 'procedure';
+  SONGID := 1;
+
+  ADD_PERSON(
+    FNAME => FNAME,
+    LNAME => LNAME,
+    SONGID => SONGID
+  );
+--rollback; 
+END;
+
