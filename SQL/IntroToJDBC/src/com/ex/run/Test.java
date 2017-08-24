c0293f5a04973fb36aa36268dc422285d068b558
Genesis Bonds <genesis.bonds@revature.com>
2017-08-24 17:14:58 -0400

    adding personal ex and class ex, will clean up later. adding for reference

--- SQL/IntroToJDBC/src/com/ex/run/Test.java
+++ SQL/IntroToJDBC/src/com/ex/run/Test.java
@@ -1,14 +1,28 @@
 package com.ex.run;
 
+import java.util.ArrayList;
+
 import com.ex.dao.DAOImpl;
+import com.ex.pojos.Example;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
 		DAOImpl dao = new DAOImpl();
-		int id = dao.getID("stored");
-		System.out.println(id);
+//		int id = dao.getID("stored");
+//		System.out.println(id);
+		
+		// testing add to db
+//		int x = dao.addExample("wow this is", "awesome");
+//		System.out.println(x);
+		
+		
+		// test get all elements
+		ArrayList<Example> list = dao.getAll();
+		for(Example ex:list){
+			System.out.println(ex.toString());
+		}
 
 	}
 
