7585c6098f229ed64763bd8363c5228690841f4c
Andy Zheng <AndyZhengJinQing@gmail.com>
2017-08-24 11:47:02 -0400

    Andy Zheng add day 9 update

--- SQL/ChinookHomeworkWeek.sql
+++ SQL/ChinookHomeworkWeek.sql
@@ -6,38 +6,57 @@
 --2.1 SELECT
 --Task – Select all records from the Employee table.
 SELECT * FROM EMPLOYEE;
+
 --Task – Select all records from the Employee table where last name is King.
 SELECT * FROM EMPLOYEE WHERE LASTNAME = 'King';
+
 --Task – Select all records from the Employee table where first name is Andrew and REPORTSTO is NULL.
 SELECT * FROM EMPLOYEE WHERE FIRSTNAME = 'Andrew' AND REPORTSTO IS NULL;
 
 --2.2 ORDER BY
 --Task – Select all albums in Album table and sort result set in descending order by title.
 SELECT * FROM ALBUM ORDER BY TITLE DESC;
+
 --Task – Select first name from Customer and sort result set in ascending order by city
 SELECT FIRSTNAME FROM CUSTOMER ORDER BY CITY ASC;
 
 
 --2.3 INSERT INTO
 --Task – Insert two new records into Genre table
+INSERT INTO GENRE
+VALUES(26,'Potatoe');
+INSERT INTO GENRE
+VALUES(27, 'tacos');
 
 --Task – Insert two new records into Employee table
+INSERT INTO EMPLOYEE
+VALUES(9,'Jackson','Bob','Taco Maker',3,'20-DEC-03','14-AUG-04','321 Taco Drive W','New York','NY','USA','10004','321-323-4232','323-421-3214','taco@taco.com');
+INSERT INTO EMPLOYEE
+VALUES(10,'Makerson','Jack','Taco Maker',4,'24-JAN-03','10-AUG-04','3211 Taco Drive W','New York','NY','USA','10034','321-323-4242','323-421-3244','taco@taco1.com');
 
 --Task – Insert two new records into Customer table
-
+INSERT INTO CUSTOMER
+VALUES(60,'Patrick','Ham','Kitchen','321 Chicken Rd','New Jersey','NJ','USA','10124','321-431-4213','323-432-3214','kitchen@taco.com',9);
+INSERT INTO CUSTOMER
+VALUES(61,'Tin','Bacon','Kitchen','32134 Taco Rd','New Jersey','NJ','USA','10304','897-321-5218','562-213-6541','birds1@taco.com',10);
 
 --2.4 UPDATE
 --Task – Update Aaron Mitchell in Customer table to Robert Walter
+UPDATE CUSTOMER SET FIRSTNAME = 'Robert', LASTNAME = 'Walter' WHERE FIRSTNAME = 'Aaron' AND LASTNAME = 'Mitchell'; 
 
 --Task – Update name of artist in the Artist table “Creedence Clearwater Revival” to “CCR”
+UPDATE ARTIST SET NAME = 'CCR' WHERE NAME = 'Creedence Clearwater Revival';
 
 --2.5 LIKE
 --Task – Select all invoices with a billing address like “T%”
+SELECT * FROM INVOICE WHERE BILLINGADDRESS LIKE 'T%';
 
 --2.6 BETWEEN
 --Task – Select all invoices that have a total between 15 and 50
+SELECT * FROM INVOICE WHERE TOTAL BETWEEN 15 AND 50;
 
 --Task – Select all employees hired between 1st of June 2003 and 1st of March 2004
+SELECT * FROM EMPLOYEE WHERE HIREDATE BETWEEN '01-JUN-03' AND '01-MAR-04';
 
 --2.7 DELETE
 --Task – Delete a record in Customer table where the name is Robert Walter (There may be constraints that rely on this, find out how to resolve them).
@@ -46,18 +65,24 @@ SELECT FIRSTNAME FROM CUSTOMER ORDER BY CITY ASC;
 --	SQL Functions 6
 --In this section you will be using the Oracle system functions, as well as your own functions, to perform various actions against the database
 
+
 --3.1 System Defined Functions
 --Task – Create a function that returns the current time.
 
+
 --Task – create a function that returns the length of a mediatype from the mediatype table
+
 
 --3.2 System Defined Aggregate Functions
 --Task – Create a function that returns the average total of all invoices
 
+
 --Task – Create a function that returns the most expensive track
+
 
 --3.3 User Defined Scalar Functions
 --Task – Create a function that returns the average price of invoiceline items in the invoiceline table
+
 
 --3.4 User Defined Table Valued Functions
 --Task – Create a function that returns all employees who are born after 1968.
@@ -66,13 +91,17 @@ SELECT FIRSTNAME FROM CUSTOMER ORDER BY CITY ASC;
 --4.0 Stored Procedures 4
 --In this section you will be creating and executing stored procedures. You will be creating various types of stored procedures that take input and output parameters.
 
+
 --4.1 Basic Stored Procedure
 --Task – Create a stored procedure that selects the first and last names of all the employees.
+
 
 --4.2 Stored Procedure Input Parameters
 --Task – Create a stored procedure that updates the personal information of an employee.
 
+
 --Task – Create a stored procedure that returns the managers of an employee.
+
 
 --4.3 Stored Procedure Output Parameters
 --Task – Create a stored procedure that returns the name and company of a customer.
@@ -84,8 +113,8 @@ SELECT FIRSTNAME FROM CUSTOMER ORDER BY CITY ASC;
 
 --Task – Create a transaction that given a invoiceId will delete that invoice (There may be constraints that rely on this, find out how to resolve them).
 
---Task – Create a transaction nested within a stored procedure that inserts a new record in the Customer table
 
+--Task – Create a transaction nested within a stored procedure that inserts a new record in the Customer table
 
 
 
@@ -95,10 +124,11 @@ SELECT FIRSTNAME FROM CUSTOMER ORDER BY CITY ASC;
 --6.1 AFTER/FOR 3
 --Task - Create an after insert trigger on the employee table fired after a new record is inserted into the table.
 --CREATE TRIGGER TRIGGERONE
+
 --Task – Create an after update trigger on the album table that fires after a row is inserted in the table
 
---Task – Create an after delete trigger on the customer table that fires after a row is deleted from the table.
 
+--Task – Create an after delete trigger on the customer table that fires after a row is deleted from the table.
 
 
 --7.0 JOINS 5
@@ -125,8 +155,3 @@ ORDER BY ARTIST.NAME ASC;
 --7.5 SELF
 --Task – Perform a self-join on the employee table, joining on the reportsto column.
 
-
-
-
-
---SELECT * 
