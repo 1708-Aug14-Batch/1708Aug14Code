b95a7a850e762e86f6637eed76573a21b4f4463b
koszuta <koszutan94@gmail.com>
2017-08-25 14:45:57 -0400

    Completed through 3.1 (need to fix 2.7)

--- SQL/Workbook/2.7 DELETE.sql
+++ SQL/Workbook/2.7 DELETE.sql
@@ -2,5 +2,8 @@
 -- Delete a record in Customer table where the name
 -- is Robert Walter (There may be constraints that
 -- rely on this, find out how to resolve them).
+ALTER TABLE INVOICE
+DROP CONSTRAINT FK_INVOICECUSTOMERID;
+
 DELETE FROM CUSTOMER
 WHERE FIRSTNAME = 'Robert' AND LASTNAME = 'Walter';
