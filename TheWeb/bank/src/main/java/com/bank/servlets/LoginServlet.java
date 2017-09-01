c783c95ae93cd26d24fae9f3da6033072d11f627
Genesis Bonds <genesis.bonds@revature.com>
2017-09-01 15:37:49 -0400

    ajax intro

--- TheWeb/bank/src/main/java/com/bank/servlets/LoginServlet.java
+++ TheWeb/bank/src/main/java/com/bank/servlets/LoginServlet.java
@@ -3,6 +3,7 @@ package com.bank.servlets;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,27 @@ import com.bank.service.Service;
 import com.google.gson.Gson;
 
 public class LoginServlet extends HttpServlet {
+	
+//	@Override
+//	public void init() throws ServletException {
+//		super.init();
+//		System.out.println("initializing login servlet");
+//		
+//		
+//	}
+	
+	protected String servletConfigParam = null;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		
+		System.out.println("intializing with params");
+		this.servletConfigParam = config.getInitParameter("param");
+		String servletContextParam = getServletContext().getInitParameter("myParam");
+		System.out.println("config param = " + servletConfigParam);
+		System.out.println("context param = "  + servletContextParam);
+	}
 
 	static Service service = new Service();
 	@Override
@@ -43,6 +65,16 @@ public class LoginServlet extends HttpServlet {
 				rd.forward(request, response); // successful login 	
 			}
 		}
+		
+	
+		
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+		System.out.println("destroying login servlet");
 	}
 
 
