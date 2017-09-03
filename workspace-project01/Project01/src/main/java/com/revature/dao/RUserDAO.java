cc92d8381e2efc5567f01690c5b3998b9180f913
Will <wwunderwood93@gmail.com>
2017-09-03 19:52:45 -0400

    Project01. Added database.properties and ConnectionSingleton.

---
+++ workspace-project01/Project01/src/main/java/com/revature/dao/RUserDAO.java
@@ -0,0 +1,33 @@
+package com.revature.dao;
+
+import java.util.ArrayList;
+
+import com.revature.model.RUser;
+
+public class RUserDAO implements IRUserDAO {
+
+	@Override
+	public void create(RUser user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public RUser read(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<RUser> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(RUser user) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ workspace-project01/Project01/src/main/java/com/revature/dao/StatusDAO.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.revature.dao;
+
+import java.util.ArrayList;
+
+import com.revature.model.Status;
+
+/**
+ * @author will
+ *
+ */
+public class StatusDAO implements IStatusDAO {
+
+	/* (non-Javadoc)
+	 * @see com.revature.dao.IStatusDAO#read(java.lang.String)
+	 */
+	@Override
+	public Status read(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.revature.dao.IStatusDAO#readAll()
+	 */
+	@Override
+	public ArrayList<Status> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
