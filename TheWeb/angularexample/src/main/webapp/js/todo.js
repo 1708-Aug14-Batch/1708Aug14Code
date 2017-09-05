57f71cbc93d1c0a475b67603beb0d3c65caa8d0c
Genesis Bonds <genesis.bonds@revature.com>
2017-09-05 15:34:31 -0400

    filters

--- TheWeb/angularexample/src/main/webapp/js/todo.js
+++ TheWeb/angularexample/src/main/webapp/js/todo.js
@@ -7,22 +7,47 @@ var app = angular.module("todoApp", []);
 app.controller("todoCtrl", function($scope){
 	$scope.username = "New User";
 	$scope.items = [ 
-	               {
-	            	   action : "Complete P1",
-	            	   done: false
-	               },
-	               {
-	            	   action : "Watch GOT",
-	            	   done:false
-	               },
-	               {action: "Make dinner",
-	            	   done:false}
+	                {
+	                	action : "Complete P1",
+	                	done: false
+	                },
+	                {
+	                	action : "Watch GOT",
+	                	done:false
+	                },
+	                {action: "Make dinner",
+	                	done:false}
 
-	               ];
+	                ];
 
-	$scope.addNewItem = function(action){
+	$scope.addNewItem = function(newaction){
 
+		$scope.items.unshift(
+				{ action : newaction,
+					done: false}
+		);
+	};
+	
+	$scope.incompleteCount = function(){
+		
 	};
 
 
+});
+
+
+app.filter("checkedItems", function(){
+	return function(items, showComplete){
+		var resultArr = [];
+//		for(item in items){
+//		} // what is the difference between these loops
+		angular.forEach(items, function(item){
+			if(item.done==false || showComplete == true){
+				resultArr.push(item);
+			}
+		});
+		
+		return resultArr;
+	}
+	
 });
