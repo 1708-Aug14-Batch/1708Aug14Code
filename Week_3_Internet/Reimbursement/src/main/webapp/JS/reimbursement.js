10cb9829b202d1af0defe3bafebc912366406b7e
bondster <apbonds1@gmail.com>
2017-09-09 16:09:39 -0400

    Current Reimbursement Progress

--- Week_3_Internet/Reimbursement/src/main/webapp/JS/reimbursement.js
+++ Week_3_Internet/Reimbursement/src/main/webapp/JS/reimbursement.js
@@ -6,8 +6,22 @@
 
 var reset = 0;
 
-function getReimbursement(){
-	var selection = $(".dropdown-item :selected").text();
+
+
+$('#selectList').on('click',function(){
+	
+	var selection = $("option:selected").text();
+
+var restriction = 0;
+if (selection === "Pending Reimbursements"){
+	restriction = 1;
+}
+else if(selection === "All Reimbursements"){
+	restriction = 0;
+}
+else if(selection === "Resolved Reimbursements"){
+	restriction = 2;
+}
 	console.log(selection);
 	console.log(reset);
 	if(reset != 0){
@@ -28,57 +42,62 @@ function getReimbursement(){
 			else{
 				
 				for (var i = 0; i < accounts.length; i++){
-					
-					var table = document.getElementById("list");
-					var row = table.insertRow();
-					var fname = row.insertCell(0);
-					var lname = row.insertCell(1);
-					var rID = row.insertCell(2);
-					var status = row.insertCell(3);
-					var subDate = row.insertCell(4);
-					var ResDate = row.insertCell(5);
-					var reason = row.insertCell(6);
-					var amount = row.insertCell(7);
-					var resID = row.insertCell(8);
-					fname.innerHTML = user.firstname;
-					lname.innerHTML = user.lastname;
-					rID.innerHTML = accounts[i].r_id;
-					if(selection == "All Reimbursements"){
-						if(accounts[i].status_id === 0){
-							status.innerHTML = "Pending";
+
+					if(accounts[i].status_id === restriction ){
+						var table = document.getElementById("list");
+						var row = table.insertRow();
+						var fname = row.insertCell(0);
+						var lname = row.insertCell(1);
+						var rID = row.insertCell(2);
+						var status = row.insertCell(3);
+						var subDate = row.insertCell(4);
+						var ResDate = row.insertCell(5);
+						var reason = row.insertCell(6);
+						var amount = row.insertCell(7);
+						var resID = row.insertCell(8);
+						fname.innerHTML = user.firstname;
+						lname.innerHTML = user.lastname;
+						rID.innerHTML = accounts[i].r_id;
+						console.log(selection);
+						console.log(restriction);
+						if(selection == "All Reimbursements"){
+							if(accounts[i].status_id === 0){
+								status.innerHTML = "Pending";
+							}
+							else if(accounts[i].status_id === 1){
+								status.innerHTML = "Approved";
+							}
+							else if(accounts[i].status_id === 2){
+								status.innerHTML = "Denied";
+							}
 						}
-						else if(accounts[i].status_id === 1){
-							status.innerHTML = "Approved";
+						if(selection == "Pending Reimbursements"){
+							status.innerHTML = "Pending"
 						}
-						else if(accounts[i].status_id === 2){
-							status.innerHTML = "Denied";
+						if(selection == "Resolved Reimbursements"){
+							console.log("Resolved ID: " +accounts[i].status_id)
+							if(accounts[i].status_id === 1){
+								status.innerHTML = "Approved";
+							}
+							if(accounts[i].status_id === 2){
+								status.innerHTML = "Denied";
+							}
 						}
+
+						subDate.innerHTML = accounts[i].subDate;
+						ResDate.innerHTML = accounts[i].resDate;
+						reason.innerHTML = accounts[i].description;
+						amount.innerHTML = "$" + accounts[i].amount;
+						resID.innerHTML = accounts[i].res_id;
 					}
-					else if(selection == "Pending Reimbursements"){
-						status.innerHTML = "Pending"
-					}
-					else if(selection == "Resolved Reimbursements"){
-						if(accounts[i].status_id == 1){
-							status.innerHTML = "Approved";
-						}
-						else if(accounts[i].status_id == 2){
-							status.innerHTML = "Denied";
-						}
-					}
-					subDate.innerHTML = accounts[i].subDate;
-					ResDate.innerHTML = accounts[i].resDate;
-					reason.innerHTML = accounts[i].description;
-					amount.innerHTML = "$" + accounts[i].amount;
-					resID.innerHTML = accounts[i].res_id;
 				}
-				
 			}
 		}
 	}
 	xhr.open("GET", "UserReimbursement", true);
 	xhr.send();
 	reset = reset + 1;
-}
+});
 
 
 
