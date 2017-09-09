5920d675355bf215815ddd5771495c6e0c497cf8
bondster <apbonds1@gmail.com>
2017-09-08 23:03:44 -0400

    Added a drop down to filter reimbursements for users

--- Week_3_Internet/Reimbursement/src/main/webapp/JS/reimbursement.js
+++ Week_3_Internet/Reimbursement/src/main/webapp/JS/reimbursement.js
@@ -3,14 +3,16 @@
  */
 
 
-window.onload = function(){
-	
-	getReimbursement();
-	
-}
 
+var reset = 0;
 
 function getReimbursement(){
+	var selection = $(".dropdown-item :selected").text();
+	console.log(selection);
+	console.log(reset);
+	if(reset != 0){
+		$("td").remove();
+	}
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function(){
 		if(xhr.readyState == 4 && xhr.status == 200){
@@ -24,6 +26,7 @@ function getReimbursement(){
 			document.getElementById("accounts").style.visiblity = "hidden";
 			}
 			else{
+				
 				for (var i = 0; i < accounts.length; i++){
 					
 					var table = document.getElementById("list");
@@ -31,14 +34,37 @@ function getReimbursement(){
 					var fname = row.insertCell(0);
 					var lname = row.insertCell(1);
 					var rID = row.insertCell(2);
-					var subDate = row.insertCell(3);
-					var ResDate = row.insertCell(4);
-					var reason = row.insertCell(5);
-					var amount = row.insertCell(6);
-					var resID = row.insertCell(7);
+					var status = row.insertCell(3);
+					var subDate = row.insertCell(4);
+					var ResDate = row.insertCell(5);
+					var reason = row.insertCell(6);
+					var amount = row.insertCell(7);
+					var resID = row.insertCell(8);
 					fname.innerHTML = user.firstname;
 					lname.innerHTML = user.lastname;
 					rID.innerHTML = accounts[i].r_id;
+					if(selection == "All Reimbursements"){
+						if(accounts[i].status_id === 0){
+							status.innerHTML = "Pending";
+						}
+						else if(accounts[i].status_id === 1){
+							status.innerHTML = "Approved";
+						}
+						else if(accounts[i].status_id === 2){
+							status.innerHTML = "Denied";
+						}
+					}
+					else if(selection == "Pending Reimbursements"){
+						status.innerHTML = "Pending"
+					}
+					else if(selection == "Resolved Reimbursements"){
+						if(accounts[i].status_id == 1){
+							status.innerHTML = "Approved";
+						}
+						else if(accounts[i].status_id == 2){
+							status.innerHTML = "Denied";
+						}
+					}
 					subDate.innerHTML = accounts[i].subDate;
 					ResDate.innerHTML = accounts[i].resDate;
 					reason.innerHTML = accounts[i].description;
@@ -51,6 +77,7 @@ function getReimbursement(){
 	}
 	xhr.open("GET", "UserReimbursement", true);
 	xhr.send();
+	reset = reset + 1;
 }
 
 
