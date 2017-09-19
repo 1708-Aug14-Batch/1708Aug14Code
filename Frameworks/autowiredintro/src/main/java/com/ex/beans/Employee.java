da58442ff8d5777d92c9838621303a991d671934
Genesis Bonds <genesis.bonds@revature.com>
2017-09-19 14:44:09 -0400

    intro to autowiring

---
+++ Frameworks/autowiredintro/src/main/java/com/ex/beans/Employee.java
@@ -0,0 +1,28 @@
+package com.ex.beans;
+
+public class Employee {
+	
+	private String name;
+	
+	private Department test;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Department getDepartment() {
+		return test;
+	}
+
+	public void setDept(Department department) {
+		this.test = department;
+	}
+
+
+
+
+}
