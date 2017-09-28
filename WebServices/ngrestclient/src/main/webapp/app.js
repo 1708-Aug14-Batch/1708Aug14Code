63cff8218e9138addaed67fd4d003c65e48daa04
Genesis Bonds <genesis.bonds@revature.com>
2017-09-28 16:51:20 -0400

    consuming a RESTful Spring Web Service with AngularJS

---
+++ WebServices/ngrestclient/src/main/webapp/app.js
@@ -0,0 +1,50 @@
+angular.module('app', [])
+
+//app.service("UserService", function($scope, $http){
+//	
+//	service.getAll = function(){
+//		$http.get('http://localhost:8050/springrestservice/users').
+//		then(function(response){
+//			$scope.users = response.data;
+//			console.log(response.data);
+//		});
+//	};
+//	
+//});
+.controller('hello', function($scope, $http){
+
+	console.log("hi");
+	$http.get('http://localhost:8050/springrestservice/users').
+	then(function(response){
+		$scope.users = response.data;
+		console.log(response.data);
+	});
+	
+
+	$scope.getUser = function(){
+		console.log('in get user function');
+		$http.get('http://localhost:8050/springrestservice/users/' + $scope.id)
+		.then(function(response){
+			$scope.user = response.data;
+		});
+
+	};
+
+
+	$scope.addUser = function(){
+		console.log($scope.name);
+
+		var user = { "username" : $scope.name };
+		user = JSON.stringify(user);
+		console.log(user);
+
+		$http.post('http://localhost:8050/springrestservice/users',user)
+		.then(function(success){
+			console.log("addedUser");
+			//UserService.getAll();
+		});
+	}
+
+
+});
+
--- WebServices/springrestclient/src/main/webapp/hello.js
+++ WebServices/springrestclient/src/main/webapp/hello.js
@@ -1,7 +1,30 @@
 angular.module('demo', [])
-.controller('Hello', function($scope, $http) {
+.controller('hello', function($scope, $http) {
+	
+	
+	console.log("test");
+	
     $http.get('http://localhost:8050/springrestservice/users').
         then(function(response) {
             $scope.users = response.data;
         });
+    
+    
+    
+    $scope.getUser = function(){
+    	console.log("in get user function");
+        $http.get('http://localhost:8050/springrestservice/users/'+ $scope.id).
+        then(function(response) {
+            $scope.user = response.data;
+        });
+    };
+    
+    
+    
+    $scope.add = function(){
+    	console.log("adding user");
+    	var user = {'username' : $scope.name };
+    	user = JSON.stringify(user);
+    	console.log(user);
+    }
 });
